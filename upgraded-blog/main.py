6c677ca8da119683017364a38c774e324bfdf80e
Dominik Bednář <bednar.dominik1@gmail.com>
2025-10-18 21:38:08 +0200

    Add files via upload

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
+from datetime import date, timedelta
+import requests
 
 app = Flask(__name__)
 
+response =requests.get('https://api.npoint.io/bb22565080ae15a79a83')
+blog_posts =response.json()
+
+for post in blog_posts:
+    post.setdefault('author', 'Dominik Bednář')
+    post.setdefault('date', date.today().strftime("%Y-%m-%d"))
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", posts=blog_posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in blog_posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 
 if __name__ == "__main__":
